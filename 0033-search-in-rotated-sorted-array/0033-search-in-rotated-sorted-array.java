fc53f9c4f4209dc4aeebad73af0b1faeb01b2322
Anjali Jadhav <anjalijadhav9143@gmail.com>
2025-11-27 16:12:22 +0530

    Time: 0 ms (100%), Space: 43.9 MB (6.55%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int pivot = findpivot(nums);
+        // if you did not find pivot it means the array is not rotated
+        if (pivot == -1) {
+            // just do binary search
+            return binarysearch(nums, target, 0, nums.length - 1);
+        }
+        // if pivot is found you have 2 asc sorted arrays
+        if (nums[pivot] == target) {
+            return pivot;
+        }
+        if (target >= nums[0]) {
+            return binarysearch(nums, target, 0, pivot - 1);
+        }
+        return binarysearch(nums, target, pivot + 1, nums.length - 1);
+    }
+
+    static int findpivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+            if (arr[mid] <= arr[start]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1; // important: return -1 if no pivot found
+    }
+public int binarysearch(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
